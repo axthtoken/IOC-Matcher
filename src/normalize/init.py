65385bdd98c8c1a19963638f5532ceef1f7228c4
Authtoken <evilsanity@proton.me>
2026-01-17 13:26:32 +1000

    Create init.py

---
+++ src/normalize/init.py
@@ -0,0 +1,5 @@
+from .normalize import normalize_ioc_value, normalize_tags
+from .validators import validate_ioc
+from .enrich import enrich_ioc_value
+
+__all__ = ["normalize_ioc_value", "normalize_tags", "validate_ioc", "enrich_ioc_value"]
